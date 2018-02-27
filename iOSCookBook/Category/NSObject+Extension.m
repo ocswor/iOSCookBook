f6f6d6e7ff0252ef968ae235a34e2188291f6b5e
yijialiang <yijialiang@stormorai.com>
2018-02-27 17:37:13 +0800

    添加相关注释

--- iOSCookBook/Category/NSObject+Extension.m
+++ iOSCookBook/Category/NSObject+Extension.m
@@ -23,6 +23,13 @@
 
 @implementation NSObject (Extension)
 
+/*
+ 注意事项 http://www.tanhao.me/pieces/160626.html/
+ block触发的线程与对象释放时的线程一致,请注意后续操作的线程安全.
+ 不要在block中强引用对象,否则引用循环释放不了;
+ 不要在block中通过weak引用对象,因为此时会返回nil;
+ (根据WWDC2011,Session322对对象释放时间的描述，associated objects清除在对象生命周期中很晚才执行，通过被NSObject -dealloc方法调用的object_dispose()函数完成);
+ */
 - (void)guard_addDeallocBlock:(void(^)(void))block {
     @synchronized (self) {
         static NSString *kAssociatedKey = nil;
